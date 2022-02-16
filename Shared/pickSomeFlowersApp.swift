29cbdaaf615adabaaca46657fb80e560c6e91056
ebibibibibi <xvipea0@gmail.com>
2022-02-16 12:55:22 +0900

    Initial Commit

---
+++ Shared/pickSomeFlowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickSomeFlowersApp.swift
+//  Shared
+//
+//  Created by kotomi tahkahashi on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct pickSomeFlowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
