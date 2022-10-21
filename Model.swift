8bca65e1ce283824a7ee1b354f3a04716150fd31
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2022-10-22 01:29:14 +0900

    [ref] Linterの警告を修正。

--- Model.swift
+++ Model.swift
@@ -20,7 +20,7 @@ struct LotFrower: Identifiable {
     var isPushed: Bool = false
 }
 
-struct Flowers: Identifiable{
+struct Flowers: Identifiable {
     var id = UUID().uuidString
     var flowerName: String
     var flowerNameJ: String
