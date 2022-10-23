b07e7f5f5988e4317c431fe445e69f75f90f9126
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2022-10-23 21:13:06 +0900

    [fast] TopページのプロパティをPickSomeFlowerViewModelに移動。

--- ViewModel.swift
+++ ViewModel.swift
@@ -7,4 +7,17 @@
 
 import SwiftUI
 
-
+class PickSomeFlowerViewModel: ObservableObject {
+    @Published var flowers: [Flowers]  = [
+        Flowers(flowerName: "flower_ajisai", flowerNameJ: "あじさい"),
+        Flowers(flowerName: "flower_tullip", flowerNameJ: "チューリップ"),
+        Flowers(flowerName: "flower_asagao", flowerNameJ: "朝顔")
+    ]
+    @Published var currentFlower: Flowers = Flowers(flowerName: "flower_ajisai", flowerNameJ: "あじさい")
+    
+    init(){
+        
+    }
+    
+    
+}
